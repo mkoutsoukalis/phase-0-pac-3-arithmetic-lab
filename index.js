a96c10ac707c59663d9a587661e9ebed1c757a87
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-08-18 19:04:34 -0400

    Completed

---
+++ index.js
@@ -0,0 +1,61 @@
+function add() {
+  // we'll fill this in shortly
+}
+function subtract() {
+  // we'll fill this in shortly
+}
+function multiply() {
+  // we'll fill this in shortly
+}
+function divide() {
+  // we'll fill this in shortly
+}
+function add() {
+  return "something";
+}
+function add(a, b) {
+  return "something";
+}
+function add(a, b) {
+  return a + b;
+}
+function subtract() {
+  return "something";
+}
+function subtract(a, b) {
+  return "something";
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply() {
+  return "something";
+}
+function multiply(a, b) {
+  return "something";
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide() {
+  return "something";
+}
+function divide(a, b) {
+  return "something";
+}
+function divide(a, b) {
+  return a / b;
+}
+function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
